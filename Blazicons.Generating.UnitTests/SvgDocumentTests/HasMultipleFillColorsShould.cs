42e99c2d111c7618242edbdd4972850f08b32b8b
Kyle Herzog <holler@kyleherzog.com>
2024-04-24 17:57:18 -0500

    addressed analyzer warning

--- Blazicons.Generating.UnitTests/SvgDocumentTests/HasMultipleFillColorsShould.cs
+++ Blazicons.Generating.UnitTests/SvgDocumentTests/HasMultipleFillColorsShould.cs
@@ -8,14 +8,12 @@ public class HasMultipleFillColorsShould
     [TestMethod]
     public void ReturnFalseGivenDifferentStrokeAndFillColors()
     {
-        {
-            var svg = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><path stroke='#000000' d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z'/><path fill='#FFFFFF' d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z'/></svg>";
-            var doc = new SvgDocument(svg);
+        var svg = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><path stroke='#000000' d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z'/><path fill='#FFFFFF' d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z'/></svg>";
+        var doc = new SvgDocument(svg);
 
-            var result = doc.HasMultipleFillColors();
+        var result = doc.HasMultipleFillColors();
 
-            Assert.IsTrue(result);
-        }
+        Assert.IsTrue(result);
     }
 
     [TestMethod]
